2e2e4ad40bf6526e6368587e3a8fcd5c730467d9
Blehisner <70594435+Blehisner@users.noreply.github.com>
2020-09-15 20:07:17 -0400

    Create t1.2.py

---
+++ t1.2.py
@@ -0,0 +1,13 @@
+#Autor Ruddy Blehisner Choqueribe Huanca
+a=int(input("Ingrese a "))
+b=int(input("Ingrese b "))
+c=int(input("Ingrese c "))
+d=(b*b)-(4*a*c)
+if d>0:
+    c=0
+    print("la naturaleza de sus raices son reales y distintas")
+else:
+    if d < 0:
+        print("la naturaleza de sus raices son complejas o conjugadas")
+    else:
+        print("la naturaleza de sus raices son reales e iguales")
