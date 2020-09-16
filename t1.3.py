d350f7bd963f1a92158a9e783752bb7f9a4ecb91
Blehisner <70594435+Blehisner@users.noreply.github.com>
2020-09-15 20:22:57 -0400

    Create t1.3.py

---
+++ t1.3.py
@@ -0,0 +1,7 @@
+#Autor Ruddy Blehisner Choqueribe Huanca
+a=int(input("Ingrese tiempo disponible: "))
+b=int(input("Ingrese tiempo necesario: "))
+if b>a:
+    print("El trabajo no puede realizarse")
+else:
+    print("El tranajo puede realizarse")
