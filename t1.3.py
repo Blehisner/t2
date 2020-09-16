9be4675c5c1b62adc92458b04d4ea0ea4063092b
Blehisner <70594435+Blehisner@users.noreply.github.com>
2020-09-15 20:52:39 -0400

    Update t1.3.py

--- t1.3.py
+++ t1.3.py
@@ -1,7 +1,15 @@
 #Autor Ruddy Blehisner Choqueribe Huanca
 a=int(input("Ingrese tiempo disponible: "))
 b=int(input("Ingrese tiempo necesario: "))
-if b>a:
+h=b-int((b/100))*100
+b=int(b/100)
+h=h*3600
+m=b-int((b/100))*100
+b=int(b/100)
+m=m*60
+sum=b+m+h
+print(sum)
+if sum>=a:
     print("El trabajo no puede realizarse")
 else:
-    print("El tranajo puede realizarse")
+    print("El trabajo puede realizarse")
